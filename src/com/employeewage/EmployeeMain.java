2cfec07f2dee66c683b3146a7c3636675554ff74
anirudha mayya <anirudha.mayya@gmail.com>
2021-08-24 12:06:10 +0530

    Welcome message

---
+++ src/com/employeewage/EmployeeMain.java
@@ -0,0 +1,10 @@
+package com.employeewage;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee wage Computation program");
+
+	}
+
+}
