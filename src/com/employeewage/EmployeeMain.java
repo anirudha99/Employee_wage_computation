d30fd18b00d1a4b1e6b6342218eb071cd7941ed9
anirudha mayya <anirudha.mayya@gmail.com>
2021-08-24 12:28:36 +0530

    Use case 5: Added monthly wage computation

--- src/com/employeewage/EmployeeMain.java
+++ src/com/employeewage/EmployeeMain.java
@@ -1,30 +1,37 @@
 package com.employeewage;
 
 public class EmployeeMain {
+	
+	// Constants
+			public static final int IS_PART_TIME = 1;
+			public static final int IS_FULL_TIME = 2;
+			public static final int EMP_RATE_PER_HOUR = 20;
+			public static final int NUM_WORKING_DAYS = 20;
+
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage Computation program");
-		
-		// Constants
-		final int IS_PART_TIME =1;
-		final int IS_FULL_TIME = 2;
-		final int EMP_RATE_PER_HOUR = 20;
-		
 		//local variables
 		int Emp_Hrs = 0;
 		int Emp_Wage = 0;
-		
+		int Total_Wage = 0;
+
 		// Computation
-		int empCheck=(int) (Math.floor(Math.random()*10)%3);
-		switch (empCheck) {
-		case  IS_FULL_TIME: Emp_Hrs=16;break;
-		case IS_PART_TIME: Emp_Hrs=8;break;
-		default: Emp_Hrs=0;
+		for(int day = 0;day < NUM_WORKING_DAYS;day++)
+		{
+			int empCheck=(int) (Math.floor(Math.random()*10)%3);
+			switch (empCheck)
+			{
+			case  IS_FULL_TIME: Emp_Hrs=16;break;
+			case IS_PART_TIME: Emp_Hrs=8;break;
+			default: Emp_Hrs=0;
+			}
+
+			Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;
+			Total_Wage+=Emp_Wage;
+			System.out.println("Employee wage is "+Emp_Wage);
 		}
-		
-		Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;
-				
-		System.out.println("Employee wage is "+Emp_Wage);
+		System.out.println("Total wage is "+Total_Wage);
 	}
 
 }
