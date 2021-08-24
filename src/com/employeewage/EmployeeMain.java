b6dd5b1eb9f282170242a9bc355d2933a7656556
anirudha mayya <anirudha.mayya@gmail.com>
2021-08-24 12:12:36 +0530

    Use case 2: Daily wage calculation

--- src/com/employeewage/EmployeeMain.java
+++ src/com/employeewage/EmployeeMain.java
@@ -4,15 +4,22 @@ public class EmployeeMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage Computation program");
+		
 		// Constants
-				final int IS_FULL_TIME =1;
+		final int IS_FULL_TIME = 1;
+		final int EMP_RATE_PER_HOUR = 20;
+		
+		//local variables
+		int Emp_Hrs=0;
+		int Emp_Wage=0;
+		
+		// Computation
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck==IS_FULL_TIME)
+			Emp_Hrs = 8;
+		Emp_Wage = Emp_Hrs*EMP_RATE_PER_HOUR ;
 				
-				// Computation
-				double empCheck=Math.floor(Math.random()*10)%2;
-				if (empCheck==IS_FULL_TIME)
-					System.out.println("Employee is present");
-				else
-					System.out.println("Employee is absent");
+		System.out.println("Employee wage is "+Emp_Wage);
 	}
 
 }
