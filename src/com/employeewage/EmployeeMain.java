23b7e938854395fc16d82c13dcaba44023333c22
anirudha mayya <anirudha.mayya@gmail.com>
2021-08-24 12:43:58 +0530

    Use case 6: Added conditonal wage

--- src/com/employeewage/EmployeeMain.java
+++ src/com/employeewage/EmployeeMain.java
@@ -1,24 +1,28 @@
 package com.employeewage;
 
 public class EmployeeMain {
-	
-	// Constants
-			public static final int IS_PART_TIME = 1;
-			public static final int IS_FULL_TIME = 2;
-			public static final int EMP_RATE_PER_HOUR = 20;
-			public static final int NUM_WORKING_DAYS = 20;
 
+	// Constants
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH=100;
 
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to Employee wage Computation program");
 		//local variables
 		int Emp_Hrs = 0;
 		int Emp_Wage = 0;
 		int Total_Wage = 0;
+		int TotalEmpHrs=0;
+		int TotalWorkingDays=0;
 
 		// Computation
-		for(int day = 0;day < NUM_WORKING_DAYS;day++)
+		while(TotalEmpHrs<MAX_HRS_IN_MONTH && TotalWorkingDays<NUM_WORKING_DAYS)
 		{
+			TotalWorkingDays+=1;
 			int empCheck=(int) (Math.floor(Math.random()*10)%3);
 			switch (empCheck)
 			{
@@ -26,12 +30,14 @@ public class EmployeeMain {
 			case IS_PART_TIME: Emp_Hrs=8;break;
 			default: Emp_Hrs=0;
 			}
-
+			TotalEmpHrs+=Emp_Hrs;
 			Emp_Wage=Emp_Hrs*EMP_RATE_PER_HOUR ;
 			Total_Wage+=Emp_Wage;
 			System.out.println("Employee wage is "+Emp_Wage);
 		}
 		System.out.println("Total wage is "+Total_Wage);
+		System.out.println("Total working hours is "+TotalEmpHrs);
+		System.out.println("Total working days is "+TotalWorkingDays);
 	}
 
 }
