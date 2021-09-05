9a96724a645ae6de321b87742055c8a01ef3e7cc
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-05 19:45:08 +0530

    Added use case 10: To manage wages of multiple companies

--- src/com/employeewage/EmployeeMain.java
+++ src/com/employeewage/EmployeeMain.java
@@ -1,73 +1,77 @@
 package com.employeewage;
 
+/**
+ * @author anirudhasm Class to compute wage, attendance of employee belonging to particular company
+ * with wager per hour for that company
+ *
+ */
 public class EmployeeMain {
 
 	// Constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public final int EMP_RATE_PER_HOUR;
-	public final int NUM_WORKING_DAYS;
-	public final int MAX_HRS_IN_MONTH;
-	public final String company;
 
+	private int numOfCompany = 0;
+
+	private CompanyEmpWage[] companyempwage; // Array
+
+	EmployeeMain(){
+		companyempwage = new CompanyEmpWage[5];
+	}
 
 	/**
 	 * @param company
-	 * @param EMP_RATE_PER_HOUR
-	 * @param NUM_WORKING_DAYS
-	 * @param MAX_HRS_IN_MONTH
-	 * Parameterised constructor
+	 * @param empWagePerHour
+	 * @param maxHoursPerMonth
+	 * @param numWorkingDays
+	 * To add company
 	 */
-	public EmployeeMain(String company,int EMP_RATE_PER_HOUR,int NUM_WORKING_DAYS,int MAX_HRS_IN_MONTH)
-	{
-		this.company=company;
-		this.EMP_RATE_PER_HOUR=EMP_RATE_PER_HOUR;
-		this.NUM_WORKING_DAYS = NUM_WORKING_DAYS;
-		this.MAX_HRS_IN_MONTH=MAX_HRS_IN_MONTH;
-
+	private void addCompanyEmpWage(String company, int empWagePerHour, int maxHoursPerMonth, int numWorkingDays) {
+		companyempwage[numOfCompany] = new CompanyEmpWage(company, empWagePerHour, maxHoursPerMonth, numWorkingDays);
+		numOfCompany += 1;
 	}
 
 	//method to check attendance and calculate wage
 	public void wageAttendance() {
-		//variables
-		int Emp_Hrs = 0;
-		int Emp_Wage = 0;
-		int Total_Wage = 0;
-		int TotalEmpHrs = 0;
-		int TotalWorkingDays = 0;
+		for (int i = 0; i < numOfCompany; i++) {
 
-		while(TotalEmpHrs<MAX_HRS_IN_MONTH && TotalWorkingDays<NUM_WORKING_DAYS) {
-			int empCheck=(int) (Math.floor(Math.random()*10)%3); //random number for attendance
-			TotalWorkingDays+=1;
-			switch (empCheck) //To check if full time or part time worker
-			{
-			case  IS_FULL_TIME:
-				Emp_Hrs=16;
-				break;
-			case IS_PART_TIME:
-				Emp_Hrs=8;
-				break;
-			default: Emp_Hrs=0;
+			int totalEmpHours, totalWorkingDays, empHours;
+			totalEmpHours = totalWorkingDays = empHours = 0;
+
+
+			while(totalEmpHours < companyempwage[i].maxHoursInMonth && totalWorkingDays < companyempwage[i].numWorkingDays) {
+				int empCheck=(int) (Math.floor(Math.random()*10)%3); //random number for attendance
+				totalWorkingDays+=1;
+				switch (empCheck) //To check if full time or part time worker
+				{
+				case  IS_FULL_TIME:
+					empHours = 16;
+					break;
+				case IS_PART_TIME:
+					empHours = 8;
+					break;
+				default: empHours = 0;
+				}
+				totalEmpHours += empHours; //calculation
+				System.out.println("Day::" + totalWorkingDays + " Emp hour - " + empHours);
+
 			}
-			TotalEmpHrs+=Emp_Hrs; //calculation
-			Emp_Wage = Emp_Hrs*EMP_RATE_PER_HOUR ;
-			Total_Wage+=Emp_Wage;
-			System.out.println("Employee wage of the company - "+company+  " is :"+ Emp_Wage);
+			companyempwage[i].setTotal_Wage(totalEmpHours * companyempwage[i].empWagePerHour);
+			System.out.println(companyempwage[i]);
+
 		}
-		System.out.println("Total wage is "+ Total_Wage);
-		System.out.println("Total working hours is "+ TotalEmpHrs);
-		System.out.println("Total working days is "+ TotalWorkingDays);
 	}
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee wage Computation program");
 
-		EmployeeMain more = new EmployeeMain("More",20,20,100); //multiple object creation for different companies
-		EmployeeMain bigbazaar = new EmployeeMain("BigBazaar",22,20,105);
-		EmployeeMain dmart = new EmployeeMain("DMart",25,22,100);
-		more.wageAttendance();
-		bigbazaar.wageAttendance();
-		dmart.wageAttendance();
+		EmployeeMain employeeWageBuilder = new EmployeeMain();
+
+		employeeWageBuilder.addCompanyEmpWage("More", 20 , 100 , 20 );
+		employeeWageBuilder.addCompanyEmpWage("BigBazaar", 22 , 95 , 20 );
+
+		employeeWageBuilder.wageAttendance();
+
 	}
 }
